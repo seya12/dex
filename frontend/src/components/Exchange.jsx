8b04d1f44c02fa150f20e9ac94030444ed97f014
seya12 <lukas.seyr@gmx.at>
2022-08-29 19:49:48 +0200

    some basic structure for exchange

--- frontend/src/components/Exchange.jsx
+++ frontend/src/components/Exchange.jsx
@@ -2,8 +2,35 @@
 TODO: call smart contract and make exchange
 */
 
+import { useState } from "react";
+
 const Exchange = () => {
-  return <h1>Exchange</h1>;
+  const [trades, setTrades] = useState([
+    {
+      tokenName: "DefaultName",
+      symbol: "DFT",
+      offer: "",
+      for: "",
+    },
+    {
+      tokenName: "DefaultName",
+      symbol: "DFT",
+      offer: "",
+      for: "",
+    },
+  ]);
+
+  const showTrades = trades.map((trade) => {
+    return <p>{trade.tokenName}</p>;
+  });
+  return (
+    <>
+      <h1>Exchange</h1>
+      <h2>Available Trades:</h2>
+      <section>{showTrades}</section>
+      <h2>Offer Trade:</h2>
+    </>
+  );
 };
 
 export default Exchange;
