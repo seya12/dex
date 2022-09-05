435247267c1d4c7bb1edca31c6ca9373e2b5b532
seya12 <lukas.seyr@gmx.at>
2022-09-05 12:41:18 +0200

    add trades from react

--- backend/contracts/Trades.sol
+++ backend/contracts/Trades.sol
@@ -2,42 +2,26 @@
 
 pragma solidity ^0.8.9;
 
-import "./Trade.sol";
-
 contract Trades {
-    struct test {
-        uint number;
-        string text;
+    struct TradePartner {
+        address participant;
+        string token;
+        int amount;
     }
 
-    uint[] public arr;
-    Trade[] public trades;
-    test[] public t;
+    struct Trade {
+        TradePartner partnerOne;
+        TradePartner partnerTwo;
+        bool open;
+    }
 
-    constructor() {
-        trades.push(
-            new Trade(
-                0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9,
-                "ABC",
-                "TST",
-                1,
-                2
-            )
-        );
-        arr.push(1);
-        arr.push(2);
-        t.push(test(1, "test"));
+    Trade[] public trades;
+
+    function addTrade(Trade memory trade) external {
+        trades.push(trade);
     }
 
     function getTrades() public view returns (Trade[] memory) {
         return trades;
-    }
-
-    function getArr() public view returns (uint[] memory) {
-        return arr;
-    }
-
-    function getStruct() public view returns (test[] memory) {
-        return t;
     }
 }
--- frontend/src/components/Exchange.jsx
+++ frontend/src/components/Exchange.jsx
@@ -1,5 +1,8 @@
 /*
-TODO: call smart contract and make exchange
+TODO: 
+- call smart contract and make exchange
+- write contract address in config file and fetch it from there
+- 
 */
 import { ethers } from "ethers";
 import { useContext, useState } from "react";
@@ -30,19 +33,45 @@ const Exchange = () => {
 
   const click = async () => {
     const trades = new ethers.Contract(
-      "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+      "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
       TradesAbi.abi,
-      etherProvider
+      signer
     );
-    console.log(trades);
-    const t = await trades.getArr();
+
+    const t = await trades.getTrades();
     console.log(t);
-    const structs = await trades.getStruct();
-    console.log(structs);
-    console.log(structs[0]);
-    const s = structs[0];
-    console.log(s.text);
-    console.log(s[1]);
+    const partners = [
+      {
+        participant: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+        token: "TEST",
+        amount: 1,
+      },
+      {
+        participant: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+        token: "COUNTER",
+        amount: 10,
+      },
+    ];
+    const trade = {
+      partnerOne: partners[0],
+      partnerTwo: partners[1],
+      open: true,
+    };
+    const trans = await trades.addTrade(trade);
+    console.log(trans);
+    const confirmedTx = await trans.wait();
+    console.log(confirmedTx);
+    const abc = await trades.getTrades();
+    console.log(abc);
+    // console.log(trades);
+    // const t = await trades.getArr();
+    // console.log(t);
+    // const structs = await trades.getStruct();
+    // console.log(structs);
+    // console.log(structs[0]);
+    // const s = structs[0];
+    // console.log(s.text);
+    // console.log(s[1]);
   };
 
   return (
